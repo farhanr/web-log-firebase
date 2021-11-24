73654eec3bc54258afe108d20f8f5774ff6c8678
farhanr <fr150899@gmail.com>
2021-11-24 18:40:23 +0800

    asdsfas

--- pages/components/Chart.js
+++ pages/components/Chart.js
@@ -1,3 +1,4 @@
+import * as timezone from 'user-timezone';
 import React, { Component } from 'react'
 import axios from 'axios'
 import ReactTable from "react-table";
@@ -11,37 +12,62 @@ export default class Chart extends Component {
             loading: true
         }
     }
+
     async getLogData() {
-        const res = await axios.get('https://iotlet-default-rtdb.firebaseio.com/log.json')
-        console.log(res.data)
-        this.setState({ loading: false, log: res.data })
+        const res = await axios.get('https://iotlet-default-rtdb.firebaseio.com/logs.json')
+        // console.log(res.data);
+        let axiosResponse = Object.keys(res.data).map(key => res.data[key]);
+        let data = [];
+        for (var i = axiosResponse.length - 1; i >= 0; i--) {
+            const splitData = axiosResponse[i].split(",");
+            // kelembaban: 0, suhu: 1, epoch: 2
+            const dateTimeFormat = 'DD MMM YY,hh:mm:ss';
+            const datetime = timezone.datetime(splitData[2], dateTimeFormat);
+            const splitTimestamp = datetime.split(",");
+
+            const obj = {
+                kelembaban: splitData[0],
+                suhu: splitData[1],
+                date: splitTimestamp[0],
+                time: splitTimestamp[1]
+            }
+            data.push(obj);
+        }
+        this.setState({ loading: false, log: data });
     }
     componentDidMount() {
         this.getLogData()
     }
     render() {
-        const columns = [{
-            Header: 'Timestamp',
-            accessor: 'time',
-        }
-            , {
-            Header: 'Kelembaban',
-            accessor: 'kelembaban',
-        }
-
-            , {
-            Header: 'Suhu',
-            accessor: 'suhu',
-        }
+        console.log(this.state.log);
+        // return null;
+        const columns = [
+            {
+                Header: 'Date',
+                accessor: 'date'
+            },
+            {
+                Header: 'Time',
+                accessor: 'time',
+            },
+            {
+                Header: 'Kelembaban',
+                accessor: 'kelembaban',
+            },
+            {
+                Header: 'Suhu',
+                accessor: 'suhu',
+            }
         ]
         return (
-            <div className="bg-white rounded-md">
-                <ReactTable
-                    data={this.state.log}
-                    columns={columns}
-                />
+            <div className="m-3">
+                <div className="bg-white rounded-md">
+                    <ReactTable
+                        data={this.state.log}
+                        columns={columns}
+                    />
+                </div>
             </div>
-            
         )
     }
 }
